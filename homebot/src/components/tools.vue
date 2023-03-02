fd65c2469acad343d31b32e369a76ff243c19ae3
yypeng5 <yypeng5@iflytek.com>
2023-03-02 10:23:56 +0800

    vue

--- homebot/src/components/tools.vue
+++ homebot/src/components/tools.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import pako from 'pako';
 import { toByteArray, fromByteArray } from 'base64-js'
 import { saveAs } from 'file-saver';
+import JSZip from 'jszip'
 
 const textdata = ref(undefined)
 const decodeText = ref(undefined)
@@ -16,21 +17,30 @@ function gzipClick() {
   decodeText.value = strin
 }
 
+function isJsonString(str) {
+      try {
+          if (typeof JSON.parse(str) == "object") {
+              return true;
+          }
+      } catch(e) {
+      }
+      return false;
+  }
+
 function formatString() {
   var value = textdata.value
   value = value.replace(/\\t|\\n|\\v|\\r|\\f/g, '\\r\\n');
   decodeText.value = JSON.stringify(value)
 }
 
-function ungzipAction(mode) {
-  var value = textdata.value
+function parseEncodeContent(input, mode) {
+  var value = input
   value = value.replace(/\\\\r\\\\n/g, '')
   value = value.replace(/\\r\\n/g, '')
   value = value.replace(/\\n/g, '')
   const bytes = toByteArray(value);
   const gizpData = pako.ungzip(bytes, { to: 'string' });
-  decodeText.value = gizpData
-
+  
   var obj = JSON.parse(gizpData)
   let width = obj.width
   let height = obj.height
@@ -44,11 +54,64 @@ function ungzipAction(mode) {
   } else if (mode === 2) {
     result = negativeSequence(width, height, data)
   }
-  decodeText.value = result
+  return result
+}
+
+function handleBatchDownload() {
+  const data = ['各类地址1', '各类地址2'] // 需要下载打包的路径, 可以是本地相对路径, 也可以是跨域的全路径
+  const zip = new JSZip()
+  const cache = {}
+  const promises = []
+  data.forEach(item => {
+  const promise = getFile(item).then(data => { // 下载文件, 并存成ArrayBuffer对象
+   const arr_name = item.split("/")
+   const file_name = arr_name[arr_name.length - 1] // 获取文件名
+   zip.file(file_name, data, { binary: true }) // 逐个添加文件
+   cache[file_name] = data
+  })
+  promises.push(promise)
+  })
+ 
+  Promise.all(promises).then(() => {
+  zip.generateAsync({type:"blob"}).then(content => { // 生成二进制流
+   FileSaver.saveAs(content, "地图打包数据.zip") // 利用file-saver保存文件
+  })
+  })
+ }
+
+function parseJSONContent(jsonObj, mode) {
+  let data = jsonObj.data
+
+  let realTime = parseEncodeContent(data.mapRealTime, mode)
+  let beau = parseEncodeContent(data.mapModification, mode)
+  let furn = parseEncodeContent(data.mapFurnitures, mode)
+  let mat = parseEncodeContent(data.mapMaterial, mode)
+  let stain = parseEncodeContent(data.mapStain, mode)
+  let obs = parseEncodeContent(data.mapObstacles, mode)
+
+  const zip = new JSZip()
+  zip.file('临时地图.txt', toByteArray(realTime), { binary: true }) // 逐个添加文件
+  
+  zip.generateAsync({type:"blob"}).then(content => { // 生成二进制流
+   FileSaver.saveAs(content, "地图打包数据.zip") // 利用file-saver保存文件
+  })
+}
+
+function ungzipAction(mode) {
+  var value = textdata.value
+
+  let isJson = isJsonString(value)
+  if (isJson) {
+    parseJSONContent(JSON.parse(value), mode)
+  } else {
+    let result = parseEncodeContent(value, mode)
+    decodeText.value = result
+  }
 }
 // 正序格式化
 function positiveSequence(width, height, data) {
   var result = ""
+  result = "width: " + width + "\n" + "height:" + height + "\n"
   let maxX = width - 1
   let maxY = height - 1
   for (var y = 0; y < maxY; y++) {
@@ -69,6 +132,7 @@ function positiveSequence(width, height, data) {
 // 反序格式化
 function negativeSequence(width, height, data) {
   var result = ""
+  result = "width: " + width + "\n" + "height:" + height + "\n"
   let maxX = width - 1
   let maxY = height - 1
   for (var y = maxY; y >= 0; y--) {
@@ -128,13 +192,34 @@ function removeAllBlankLines(value) {
   return result
 }
 
+function ab2str(buf) {
+   return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
+ 
+// 字符串转为ArrayBuffer对象，参数为字符串
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length*2); // 每个字符占用2个字节
+    var bufView = new Uint16Array(buf);
+    for (var i=0, strLen=str.length; i<strLen; i++) {
+         bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
 function saveTxt() {
   let str = decodeText.value
   if (str.length <= 0) {
     alert
   }
-  let strData = new Blob([str], { type: 'text/plain;charset=utf-8' });
-  saveAs(strData, "测试文件下载.txt");
+  // let strData = new Blob([str], { type: 'text/plain;charset=utf-8' });
+  // saveAs(strData, "测试文件下载.txt");
+
+  const zip = new JSZip()
+  zip.file('临时地图.txt', str2ab(str), { binary: true }) // 逐个添加文件
+  
+  zip.generateAsync({type:"blob"}).then(content => { // 生成二进制流
+   FileSaver.saveAs(content, "地图打包数据.zip") // 利用file-saver保存文件
+  })
 }
 
 function handleFileInput() {
